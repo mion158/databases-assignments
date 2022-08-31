7ff09d14f215a12085f81e9328d17ab66eddd7b6
mion158 <111487792+mion158@users.noreply.github.com>
2022-08-31 22:50:36 +0100

    Update app.py

--- jinja2-and-forms/app.py
+++ jinja2-and-forms/app.py
@@ -3,7 +3,7 @@ from locations import Locations
 from forms import AddLocationForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET_PROJECT'
+app.config['SECRET_KEY'] = 'nosecret'
 
 visit = Locations()
 categories = {"recommended": "Recommended", "tovisit": "Places To Go", "visited": "Visited!!!", }
@@ -15,19 +15,21 @@ DEL_ACTION = "X"
 def locations(category):
   locations = visit.get_list_by_category(category)
   ## Check the request for form data and process
-  if request.method == "POST" :
+  if request.method == 'POST':
     [(name, action)] = request.form.items()
+
     if action == UP_ACTION:
       visit.moveup(name)
     elif action == DEL_ACTION:
       visit.delete(name)
   ## Return the main template with variables
-  return render_template("locations.html", category=category, categories=categories, locations=locations, add_location=AddLocationForm())
+  return render_template('locations.html',category=category, categories=categories, locations=locations, add_location=AddLocationForm())
 
 @app.route("/add_location", methods=["POST"])
 def add_location():
   ## Validate and collect the form data
   add_form = AddLocationForm()
+
   if add_form.validate_on_submit:
       name=add_form.name.data
       description=add_form.description.data
@@ -35,10 +37,10 @@ def add_location():
       visit.add(name, description, category)
 
   ## Redirect to locations route function
-  return redirect(url_for("locations",category=category,_external=True,_scheme='https'))
+  return redirect(url_for('locations',category=category, _external=True, _scheme='https'))
 
-#still updating
 @app.route("/")
 def index():
+
   ## Redirect to locations route function
-  return redirect(url_for("location",category="recommended",_external=True,_scheme='https'))
+  return redirect(url_for('location', category='recommended', _external=True, _scheme='https'))
